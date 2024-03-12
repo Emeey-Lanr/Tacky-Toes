d83c65cb8a4c86603d9d65d504584c692d3445e6
Emeey-Lanr <emeeylanr04@gmail.com>
2024-03-12 13:41:31 +0100

    change d url

--- appContext/MainAppContext.tsx
+++ appContext/MainAppContext.tsx
@@ -14,6 +14,7 @@ import { collectUserDetailsR } from "@/Redux/Constituents/User"
 import { useSocket } from "@/socket"
 import { notificationOnloadR } from "@/Redux/Constituents/Notification"
 
+
 // Create one function for deleteModal also
  export const appContext  = createContext(appContextSchema)
 export const MainAppContext = ({ children }: { children: React.ReactNode }) => {
