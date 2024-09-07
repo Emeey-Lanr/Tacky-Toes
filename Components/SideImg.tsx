f3d38a4d41199d3c5adfbf3af7f4fe87eb06bc9e
Oyelowo Emmanuel <108054902+Emeey-Lanr@users.noreply.github.com>
2024-09-07 04:29:23 +0100

    Delete Components directory

--- Components/SideImg.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import sideImg from "@/public/icon/nav-side-icon.svg";
-const SideImg = () => {
-  return <Image src={sideImg} alt="side img" />;
-};
-
-export default SideImg;
